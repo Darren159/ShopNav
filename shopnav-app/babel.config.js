4e483b6399089af40b5023ad48f0d301dda8b98d
adammangzijun <117640074+adammangzijun@users.noreply.github.com>
2023-07-21 15:25:31 +0800

    fix: removed extra nexted shopnav-app

--- shopnav-app/babel.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line func-names
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: ["expo-router/babel", "react-native-reanimated/plugin"],
-  };
-};
