61c204d0f15619f5e456961712cf0f413818963f
adammangzijun <117640074+adammangzijun@users.noreply.github.com>
2023-07-24 17:35:55 +0800

    fix: adding APi to env, bypass husky

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,18 +4,19 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 import { getAuth } from "firebase/auth";
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from "@env";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCq78MIOaG7CXSTWRlDVYQOSzniNdkiqqU",
-  authDomain: "shopnav-33436.firebaseapp.com",
-  projectId: "shopnav-33436",
-  storageBucket: "shopnav-33436.appspot.com",
-  messagingSenderId: "644946336222",
-  appId: "1:644946336222:web:1fc761de2dde100d51e54f",
-  measurementId: "G-KSP3YFNMW0",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
